843d9409c32d136357c5009b23bb6771a1572a5f
Jian-Long Huang <huang@jianlong.org>
2019-06-15 02:59:32 +0800

    Fix syntax error

--- script/kepub2cbz.rb
+++ script/kepub2cbz.rb
@@ -8,7 +8,7 @@ Dir["#{input_dir}/**/*.kepub.epub"].each do |f|
   filename = File.basename f
   basename = File.basename(f, ".kepub.epub") + ".cbz"
   Dir.mktmpdir do |dir|
-    `cp #{f} #{dir}`
+    `cp '#{f}' #{dir}`
     `cd #{dir} && unzip '#{dir}/#{filename}'`
     `rm -f #{dir}/image/createby.png #{dir}/image/logo-mark.png`
     `zip -r -j '#{output_path}/#{basename}' #{dir}/image`
