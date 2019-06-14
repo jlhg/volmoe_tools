bcc292d8e59b36a75c3b918521a4003aed3bdb86
Jian-Long Huang <huang@jianlong.org>
2019-06-15 02:15:33 +0800

    Rename to cbz after conversion

--- script/kepub2cbz.rb
+++ script/kepub2cbz.rb
@@ -6,6 +6,8 @@ output_dir = ARGV[1]
 output_path = File.expand_path output_dir
 Dir["#{input_dir}/**/*.kepub.epub"].each do |f|
   input_path = File.expand_path f
-  output_filename = File.basename(f, ".kepub.epub") + ".cbz"
+  output_filename = File.basename(f, ".kepub.epub") + ".zip"
+  output_filename_cbz = File.basename(f, ".kepub.epub") + ".cbz"
   `docker run --rm -v #{input_path}:/input.kepub.epub -v #{output_path}:/output jlhg/calibre-docker ebook-convert /input.kepub.epub '/output/#{output_filename}'`
+  `mv '#{output_path}/#{output_filename}' '#{output_path}/#{output_filename_cbz}'`
 end
