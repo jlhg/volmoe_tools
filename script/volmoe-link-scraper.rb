8f27f215e2faa861551cd8e174058c5eb6b6e660
Jian-Long Huang <huang@jianlong.org>
2019-06-18 01:42:28 +0800

    Revert "Add --save-cookies to wget"
    This reverts commit 4bbea566b78acb6a7334e6aafcca3d9a80c16ff0.

--- script/volmoe-link-scraper.rb
+++ script/volmoe-link-scraper.rb
@@ -6,7 +6,7 @@ format = ARGV[0]
 url = ARGV[1]
 cookies_path = ARGV[2] || "cookies.txt"
 
-html = `wget -O- -x --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' --no-check-certificate --content-disposition --load-cookies #{cookies_path} --save-cookies #{cookies_path} '#{url}'`
+html = `wget -O- -x --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' --no-check-certificate --content-disposition --load-cookies #{cookies_path} '#{url}'`
 
 case format
 when "epub"
