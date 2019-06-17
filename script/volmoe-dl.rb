edc8e26ca6eeddbfa7c21229c0ad5a2edde06fa9
Jian-Long Huang <huang@jianlong.org>
2019-06-18 02:08:53 +0800

    Wait for termination

--- script/volmoe-dl.rb
+++ script/volmoe-dl.rb
@@ -15,3 +15,4 @@ CSV.foreach(url_path, col_sep: "\t", headers: false) do |row|
     `wget -x --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' --no-check-certificate --content-disposition --load-cookies #{cookies_path} -P #{output_dir} '#{url}'`
   end
 end
+pool.wait_for_termination
