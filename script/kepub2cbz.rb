08766d92b05cca47db11d7d86b01bc4ee41d32e7
Jian-Long Huang <huang@jianlong.org>
2019-06-18 02:24:39 +0800

    kepub2cbz: support concurrency

--- script/kepub2cbz.rb
+++ script/kepub2cbz.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/env ruby
 require "tempfile"
 require "fileutils"
+require "concurrent"
 
 input_dir = ARGV[0]
 output_dir = ARGV[1]
 FileUtils.mkdir_p(output_dir)
 output_path = File.expand_path output_dir
+pool = Concurrent::FixedThreadPool.new(8)
 Dir["#{input_dir}/**/*.kepub.epub"].each do |f|
-  filename = File.basename f
-  basename = File.basename(f, ".kepub.epub") + ".cbz"
-  Dir.mktmpdir do |dir|
-    `cp '#{f}' #{dir}`
-    `cd #{dir} && unzip '#{dir}/#{filename}'`
-    `rm -f #{dir}/image/createby.png #{dir}/image/logo-mark.png`
-    `zip -r -j '#{output_path}/#{basename}' #{dir}/image`
-    puts "converted: #{f}"
+  pool.post do
+    filename = File.basename f
+    basename = File.basename(f, ".kepub.epub") + ".cbz"
+    Dir.mktmpdir do |dir|
+      `cp '#{f}' #{dir}`
+      `cd #{dir} && unzip '#{dir}/#{filename}'`
+      `rm -f #{dir}/image/createby.png #{dir}/image/logo-mark.png`
+      `zip -r -j '#{output_path}/#{basename}' #{dir}/image`
+      puts "converted: #{f}"
+    end
   end
 end
+pool.shutdown
+pool.wait_for_termination
