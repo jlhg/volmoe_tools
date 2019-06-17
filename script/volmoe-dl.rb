4bbea566b78acb6a7334e6aafcca3d9a80c16ff0
Jian-Long Huang <huang@jianlong.org>
2019-06-18 01:39:13 +0800

    Add --save-cookies to wget

--- script/volmoe-dl.rb
+++ script/volmoe-dl.rb
@@ -9,5 +9,5 @@ cookies_path = ARGV[2] || "cookies.txt"
 FileUtils.mkdir_p(output_dir)
 CSV.foreach(url_path, col_sep: "\t", headers: false) do |row|
   url = row[1]
-  `wget -x --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' --no-check-certificate --content-disposition --load-cookies #{cookies_path} -P #{output_dir} '#{url}'`
+  `wget -x --user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36' --no-check-certificate --content-disposition --load-cookies #{cookies_path} --save-cookies #{cookies_path} -P #{output_dir} '#{url}'`
 end
