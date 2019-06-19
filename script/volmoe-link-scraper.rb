125c43f4c6768bd4f1dc8316d26b1627e3a1e120
Jian-Long Huang <huang@jianlong.org>
2019-06-19 20:21:27 +0800

    Fix

--- script/volmoe-link-scraper.rb
+++ script/volmoe-link-scraper.rb
@@ -17,10 +17,10 @@ when "epub"
     book1_url = td[1].css("a").select { |d| d.text == "VIP下载" }.first.attributes["href"].text
     puts [book1_title, book1_url].join("\t") if book1_title
 
-    next if td[3].nil? || td[4].nil?
+    next if td[3].nil? || td[3].css("b").empty? || td[4].nil? || td[4].css("a").empty?
 
     book2_title = td[3].css("b").text.gsub(/ +/, "")
     book2_url = td[4].css("a").select { |d| d.text == "VIP下载" }.first.attributes["href"].text
-    puts [book2_title, book2_url].join("\t") if book2_title
+    puts [book2_title, book2_url].join("\t") if book2_title && book2_url
   end
 end
