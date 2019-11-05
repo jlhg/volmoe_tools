4d70c9f15a0432284bd100eb6ae6393ed207f5c0
Jian-Long Huang <huang@jianlong.org>
2019-11-05 18:26:12 +0100

    kepub2cbz: use shellwords to escape filename

--- script/kepub2cbz.rb
+++ script/kepub2cbz.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
-require "tempfile"
 require "fileutils"
+require "shellwords"
+require "tempfile"
+
 require "concurrent"
 
 input_dir = ARGV[0]
@@ -8,16 +10,17 @@ output_dir = ARGV[1]
 FileUtils.mkdir_p(output_dir)
 output_path = File.expand_path output_dir
 pool = Concurrent::FixedThreadPool.new(8)
-Dir["#{input_dir}/**/*.kepub.epub"].each do |f|
+Dir["#{input_dir}/**/*.kepub.epub"].each do |file_path|
+  f = Shellwords.escape(file_path)
   pool.post do
-    filename = File.basename f
+    filename = File.basename(f)
     basename = File.basename(f, ".kepub.epub") + ".cbz"
     Dir.mktmpdir do |dir|
-      `cp '#{f}' #{dir}`
-      `cd #{dir} && unzip '#{dir}/#{filename}'`
+      `cp #{f} #{dir}`
+      `cd #{dir} && unzip #{dir}/#{filename}`
       `rm -f #{dir}/image/createby.png #{dir}/image/logo-mark.png`
-      `zip -r -j '#{output_path}/#{basename}' #{dir}/image`
-      puts "converted: #{f}"
+      `zip -r -j #{output_path}/#{basename} #{dir}/image`
+      puts "converted: #{file_path}"
     end
   end
 end
